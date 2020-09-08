251663782e73b81cae94340631740d24e23e8fdb
liv <olivia.f.smith@gmail.com>
2020-09-08 16:06:46 -0700

    slk

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const rectangle = require('./rectangle'); //require it
+const rect = require('./rectangle'); //require it
 
 // console.log(rectangle.name);
 // Use the module's functions (defined in module.exports)
@@ -7,12 +7,14 @@ const rectangle = require('./rectangle'); //require it
 
 // https://www.coursera.org/learn/server-side-nodejs/lecture/dUnyG/exercise-video-understanding-node-modules
 
-// Create a JavaScript Object
+// First, Create a JavaScript Object named rect and assign the function names to the JS Object.
+// This will allow you to do these operations on the object.
 var rect = {
     perimeter: (x, y) => rectangle.perimeter(x, y),
     area: (x, y) => rectangle.area(x, y),
 }
 
+// Costly: Solve the 'normal' way (without a callback) 
 function solveRect(l, b) {
     // Takes length and breadth of retangle, computes rectangle
     console.log("Solving for rectangle l/w: " + l + w);
@@ -26,24 +28,36 @@ function solveRect(l, b) {
     }
 }
 
-
+// Better: Solve the Callback Function way -- a common pattern in Node apps, including error handling
 function solveRectCallbackWay(l, b) {
     // Same idea as above, but uses a callback for error handling
     // This allows the operation to occur async
     console.log("Solving for rectangle with l = " + l + " and b = " + b);
 
-    rectangle(l, b, (err, rectangle) => {
+    // Call the rectangle module, and set the l, b, and callback function which has two parameters
+    // This callback function is implemented as follows
+    // Inside this callback function, you can check to see if these values are returned to you
+    // If the error is returned to you, do a console log 
+    // If not, we know the rectangle was returned to you so can go ahead and do the operations specified
+
+    // Here we are defining an inner object
+    
+    rectangle(l, b, (err, recta) => {
         if (err) {
             console.log("Error: ", err.message);
         }
         else {
+            // We don't have to assign any parameters to these function calls
+            // Because of the concept of CLOSURE, the l and b become x, y
+            // They are automatically retrieved from the items that are already created (JS object)
+            // The call 
             console.log("The area of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.area());
+                + l + " and b = " + b + " is " + recta.area());
             console.log("The perimeter of the rectangle of dimensions l = "
-                + l + " and b = " + b + " is " + rectangle.perimeter());
+                + l + " and b = " + b + " is " + recta.perimeter());
         }
     });
-    // console.log("This statement after the call to rect()");
+    console.log("This statement after the call to rect()");
 
 
 };
