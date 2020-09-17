b5440b2a7616904d19536b3abe95c2212628f7a5
liv <olivia.f.smith@gmail.com>
2020-09-16 19:00:14 -0700

    thing

---
+++ node_http_server.js
@@ -0,0 +1,16 @@
+// Using "Core" Node.js modules to build an HTTP Server
+// https://www.coursera.org/learn/server-side-nodejs/supplement/75pR8/exercise-instructions-node-and-the-http-module
+
+// Import modules
+const http = require('http');
+
+
+// Constructor Function: Takes request and response
+const server = http.createServer((req, res) =)
+
+
+
+// Start the server 
+server.listen(port, hostname);
+
+
